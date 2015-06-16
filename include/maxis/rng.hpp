557a7cdc1e56fa2adb5954b1af7c04565ef0a123
Dylan MacKenzie <dylanxmackenzie@gmail.com>
2015-06-15 21:01:45 -0700

    Remove inlining of RNG methods
    The compiler will do this for us.

--- include/maxis/rng.hpp
+++ include/maxis/rng.hpp
@@ -1,9 +1,10 @@
 #include <random>
 
-#ifndef MAXIS_RNG_HPP
-#define MAXIS_RNG_HPP
+#ifndef MAXIS_RNG_H
+#define MAXIS_RNG_H
 
-// Mixin for seeding and generating random doubles between 0 and 1
+// Mixin for seeding and generating random doubles between 0 and 1 and
+// random indexes from start to end inclusive
 class RNG {
 public:
     RNG() : engine{0}, probability{0, 1} {};
@@ -11,8 +12,8 @@ public:
     template<typename SeedType>
     void seed_random(SeedType seed) { engine.seed(seed); };
 
-    inline double random_prob() { return probability(engine); };
-    inline size_t random_index(size_t start, size_t end) {
+    double random_prob() { return probability(engine); };
+    size_t random_index(size_t start, size_t end) {
         std::uniform_int_distribution<size_t> range{start, end};
         return range(engine);
     };
