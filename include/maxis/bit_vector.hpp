2d379e75343cceb46b31ae68e78ca134afff4ca8
Dylan MacKenzie <dylanxmackenzie@gmail.com>
2015-06-22 19:55:06 -0700

    Add a const_bitset_iterator and fix distance_to
    We change bitset_iterator into a templated class that can use both a
    dynamic_bitset::const_reference or a dynamic_bitset::reference. This
    will allow us to use const in the appropriate places. Also, we fix the
    incorrectly specified distance_to function.

--- include/maxis/bit_vector.hpp
+++ include/maxis/bit_vector.hpp
@@ -9,40 +9,58 @@ namespace maxis {
 // Could be any random access container including vector<bool>
 using BitVector = boost::dynamic_bitset<>;
 
-class bitset_iterator :
-    public boost::iterator_facade<
-        bitset_iterator,
-        bool,
-        boost::random_access_traversal_tag,
-        boost::dynamic_bitset<>::reference,
-        long int >
-    {
+namespace detail {
+    // TODO: long int may not be large enough to hold a distance value
+    // in all cases. Find a suitable replacement.
+    template <typename ReferenceType>
+    class bitset_iterator :
+        public boost::iterator_facade<
+            bitset_iterator<ReferenceType>,
+            bool,
+            boost::random_access_traversal_tag,
+            ReferenceType,
+            long int>
+        {
 
-public:
-    bitset_iterator() : b{nullptr}, pos{0} {}
-    bitset_iterator(boost::dynamic_bitset<>* b, boost::dynamic_bitset<>::size_type i = 0) : b{b}, pos{i} {}
+    public:
+        bitset_iterator() : b{nullptr}, pos{0} {}
+        bitset_iterator(boost::dynamic_bitset<>* b, boost::dynamic_bitset<>::size_type i = 0)
+            : b{b}, pos{i} {}
 
-private:
-    friend class boost::iterator_core_access;
+        template <typename OtherReferenceType>
+        bitset_iterator(const bitset_iterator<OtherReferenceType> &other)
+            : b{other.b}, pos{other.pos} {}
 
-    boost::dynamic_bitset<> *b;
-    boost::dynamic_bitset<>::size_type pos;
+    private:
+        friend class boost::iterator_core_access;
 
-    boost::dynamic_bitset<>::reference dereference() const { return (*b)[pos]; }
-    bool equal(bitset_iterator const& other) const {
-        return this->b == other.b && this->pos == other.pos;
-    }
-    void increment() { ++pos; }
-    void decrement() { --pos; }
-    void advance(long int n) { pos += n; }
-    long int distance_to(boost::dynamic_bitset<>::size_type z) const { return pos - z; }
-};
+        boost::dynamic_bitset<> *b;
+        boost::dynamic_bitset<>::size_type pos;
+
+        ReferenceType dereference() const { return (*b)[pos]; }
+        void increment() { ++pos; }
+        void decrement() { --pos; }
+        void advance(long int n) { pos += n; }
+
+        template <typename OtherReferenceType>
+        bool equal(const bitset_iterator<OtherReferenceType> &other) const {
+            return this->b == other.b && this->pos == other.pos;
+        }
+
+        template <typename OtherReferenceType>
+        long int distance_to(const bitset_iterator<OtherReferenceType> &other) const {
+            return this->pos - other.pos;
+        }
+    };
+} // namespace detail
+
+using bitset_iterator = detail::bitset_iterator<boost::dynamic_bitset<>::reference>;
+using const_bitset_iterator = detail::bitset_iterator<boost::dynamic_bitset<>::const_reference>;
 
 // Overload begin and end for a boost::dynamic_bitset in our local
 // namespace. Becasue most code uses using declarations for std::begin
 // and std::end, these will be called instead of their standard library
 // counterparts.
-
 auto begin(boost::dynamic_bitset<> &b) -> maxis::bitset_iterator;
 auto end(boost::dynamic_bitset<> &b) -> maxis::bitset_iterator;
 
